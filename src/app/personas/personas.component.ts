24802db74151c6a589629b3ef9aa1105f01d5939
bolijesus <bolijesus98@gmail.com>
2023-02-22 22:29:07 -0500

    Se vio el concepto de Event Binding
    https://angular.io/guide/event-binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -3,15 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  // styleUrls: ['./personas.component.css']
-  styles:[`
-    h1{
-      color: red
-    }
-  `]
+  styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent {
 
-  deshabilitar:boolean = true
+  deshabilitar:boolean = false;
+  mensaje:string = 'No se ha agregado ninguna persona';
+  titulo:string = '';
 
+  agregarPersona(){
+    this.mensaje = 'Persona agregada';
+  }
+
+  modificarTitulo(event: Event){
+    this.titulo = (<HTMLInputElement>event.target).value;
+  }
 }

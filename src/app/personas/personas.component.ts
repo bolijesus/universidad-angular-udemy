875a348f2c0fd8b4ea7f90e48fd62c604dd52a76
bolijesus <bolijesus98@gmail.com>
2023-02-22 20:52:35 -0500

    Creacion de template en linea
    Se realizo un template en linea sustituyendo la propiedad templateUrl:,
    por template:

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html',
+  template: `<h1>Listado de personas</h1>
+    <app-persona></app-persona>
+    <app-persona></app-persona>`,
 })
-export class PersonasComponent{
-
-}
+export class PersonasComponent {}

04ca47895139416f695824eb138f58527780c1b0
bolijesus <bolijesus98@gmail.com>
2023-02-28 21:37:29 -0500

    Inyectar un servicio dentro de otro servicio

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,12 +1,20 @@
+import { LogginService } from "./LogingService.service";
 import { Persona } from "./persona.model";
+import {Injectable} from "@angular/core"
 
+@Injectable()
 export class PersonasService {
     personas: Persona[] = [
         new Persona('Juan', 'Perez'),
         new Persona('Laura', 'Juarez'),
     ];
 
+    constructor(private logingService:LogginService){
+
+    }
+
     agregarPersona(persona: Persona): void {
+        this.logingService.enviaMensajeAConsola("agregamos persona: "+persona);
         this.personas.push(persona);
     }
 }

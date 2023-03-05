7ba904e8a323a9856339234c92b8d4a9af481527
bolijesus <bolijesus98@gmail.com>
2023-03-05 09:32:23 -0500

    Se realizo el componente para agregar y eliminar los ingresos

--- src/app/ingreso/ingreso.servicio.ts
+++ src/app/ingreso/ingreso.servicio.ts
@@ -5,4 +5,9 @@ export class IngresoServicio{
         new Ingreso('Salario', 4000),
         new Ingreso('Venta Coche', 500),
     ];
+
+    eliminar(ingreso:Ingreso){
+        const indeice:number = this.ingresos.indexOf(ingreso);
+        this.ingresos.splice(indeice, 1);
+    }
 }

465916cb6f36f2798ef0f70e4869005a859141a5
bolijesus <bolijesus98@gmail.com>
2023-03-05 09:21:52 -0500

    Se realizó el componente de Cabecero
    Se mandó los parametros que estan en app.componente hacia
    cabecero.component usando @Input()

--- src/app/cabecero/cabecero.component.ts
+++ src/app/cabecero/cabecero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cabecero',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./cabecero.component.css']
 })
 export class CabeceroComponent {
+  //@Input es para enviar informacion de otro componente hacia el atributo
+  @Input() presupuestoTotal:number = 0; // se esta recibiendo de otro componente hacia este y se guarda en la variable presupuestoTotal
+  @Input() ingresoTotal:number = 0;
+  @Input() egresoTotal:number = 0;
+  @Input() porcentajeTotal:number = 0;
+
 
 }
--- src/app/ingreso/ingreso.servicio.ts
+++ src/app/ingreso/ingreso.servicio.ts
@@ -1,7 +1,7 @@
 import { Ingreso } from "./ingreso.model";
 
 export class IngresoServicio{
-    ingreso:Ingreso[] = [
+    ingresos:Ingreso[] = [
         new Ingreso('Salario', 4000),
         new Ingreso('Venta Coche', 500),
     ];

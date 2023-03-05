573be85a881d0639957841aa45ec14a455bce4e9
bolijesus <bolijesus98@gmail.com>
2023-03-05 13:44:21 -0500

    Se vio el concepto de rutas hisjas en alngular child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
 
-const routes:Routes = [
-  {path: '', component: PersonasComponent},
-  {path: 'personas', component: PersonasComponent},
-  {path: 'personas/agregar', component: FormularioComponent},
-  {path: 'personas/:id', component: FormularioComponent}
+const routes: Routes = [
+  { path: '', component: PersonasComponent },
+  {
+    path: 'personas', component: PersonasComponent,
+    children: [
+      { path: 'agregar', component: FormularioComponent },
+      { path: ':id', component: FormularioComponent }
+    ]
+  }
 ];
 
 @NgModule({
